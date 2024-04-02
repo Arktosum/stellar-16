0307ae2a46be3dcc358164ee821ac2ae3b58f820
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-04-02 14:38:20 +0530

    fix : MSJKFF was not zero by default

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Stellar-16
- 
+
+# Instruction Cycle
+    - Time taken to Fetch and Execute an Instruction
+    - Instruction cycle takes multiple clock cycles.
+  
+## Phases of Instruction Cycle:
+    - Fetch: The CPU retrieves the instruction from memory.
+    - Decode: The CPU decodes the instruction to determine the operation to be performed and the operands involved.
+    - Execute: The CPU performs the operation specified by the instruction, which may involve arithmetic, logical, or data transfer operations.
+    - Writeback: In architectures with a separate writeback phase, the CPU updates the result of the operation in the appropriate register or memory location.
+  
+![alt text](image-1.png)
+![alt text](image.png)
+> so.. T-states are just fancy lingo for one clock period. but it talks about the thing executed in that clock period
+### Machine cycle = SUM(T states)
+> Machine cycle = One phase of Instruction cycle.
+> Instruction cycle = SUM(Machine cycles) = FETCH + DECODE + EXECUTE + WRITEBACK
+
+
+## TRI STATE LOGIC
+![alt text](image-2.png)
+buses always need to be protected!
--- main.py
+++ main.py
@@ -1,44 +1,115 @@
 import time
 
-from gates import Register16
+from gates import MemoryUnit, Number
 
-class RAM:
+class Component:
     def __init__(self):
-        self.data = []
-        for _ in range(2**16):
-            self.data.append(Register16())
+        pass
+    def update(self):
+        pass
+    def readFromBus(self):
+        pass
+    def writeToBus(self):
+        pass
+
+class Bus:
+    def __init__(self):
+        self.data = Number(0)
+    def write(self, data:Number):
+        self.data = data
+    def read(self) -> Number:
+        return self.data
+    def print(self,showQ = True):
+        self.data.print(16)
+        
+        
+class Register(Component):
+    def __init__(self,BUS : Bus):
+        super().__init__()
+        self.READ_ENABLE = False
+        self.WRITE_ENABLE = False
+        self.BUS = BUS
+        self.flipflops = [MemoryUnit() for _ in range(16)]
+    def CONTROL_READ(self,value:bool):
+        self.READ_ENABLE = value
+    def CONTROL_WRITE(self,value:bool):
+        self.WRITE_ENABLE = value
+    def readFromBus(self,CLOCK):
+        if not self.READ_ENABLE:
+            return
+        self.write(self.BUS.read(),CLOCK)
+        
+    def writeToBus(self):
+        if not self.WRITE_ENABLE:
+            return
+        self.BUS.write(self.read())
+        
+    def write(self, data:Number,CLOCK):
+        binArray = data.tobinaryArray(16)
+        for i in range(len(binArray)):
+            self.flipflops[i].DFF(binArray[i],CLOCK)
+    def read(self,showQ = True) -> Number:
+        array = [ff.Q if showQ else ff.Q_ for ff in self.flipflops]
+        val = Number.fromBinaryArray(array)
+        return val
+    def print(self,showQ = True):
+        self.read(showQ).print(16)
+        
 class Computer:
     def __init__(self,clockRate):
         self.clock = False
         self.clockRate = clockRate
+        self.BUS = Bus()
+        self.HALT = False
+        self.A_REGISTER = Register(self.BUS)
         
-        self.RAM = RAM()
+        self.A_REGISTER.write(Number(159),True)
+        self.B_REGISTER = Register(self.BUS)
+        self.C_REGISTER = Register(self.BUS)
+        self.INSTRUCTION_REGISTER = Register(self.BUS)
+        self.STACK_POINTER = Register(self.BUS)
+        self.COMPONENTS = [
+            self.A_REGISTER,self.B_REGISTER,self.C_REGISTER,
+            self.INSTRUCTION_REGISTER,self.STACK_POINTER
+        ]
         
-        self.PC = Register16()
-        self.IR = Register16()
-    
-        self.ACCUMULATOR = Register16()
-        self.B_REGISTER = Register16()
+    def update(self):
+        # CONTROL WORD
+        # AREAD,AWRITE,BREAD,BWRITE,CREAD,CWRITE,IRREAD,IRWRITE,SPREAD,SPWRITE
+        control_word = [
+            self.A_REGISTER.CONTROL_READ,
+            self.A_REGISTER.CONTROL_WRITE,
+            self.B_REGISTER.CONTROL_READ,
+            self.B_REGISTER.CONTROL_WRITE,
+            self.C_REGISTER.CONTROL_READ,
+            self.C_REGISTER.CONTROL_WRITE,
+            self.INSTRUCTION_REGISTER.CONTROL_READ,
+            self.INSTRUCTION_REGISTER.CONTROL_WRITE,
+            self.STACK_POINTER.CONTROL_READ,
+            self.STACK_POINTER.CONTROL_WRITE,
+            ]
+        print(self.clock)
+        control_input = [x=="1" for x in input("Instruction word : ")]
         
-    def lowLevel(self):
-        print("-- Low level -- clock : " + str("HIGH" if self.clock else 'LOW'))
-    def risingEdge(self):
-        print("-- Rising edge -- clock : " + str("HIGH" if self.clock else 'LOW'))
-    def highLevel(self):
-        print("-- High level -- clock : " + str("HIGH" if self.clock else 'LOW'))
-    def fallingEdge(self):
-        print("-- Falling edge -- clock : " + str("HIGH" if self.clock else 'LOW'))
-    
+        for i in range(len(control_input)):
+            control_word[i](control_input[i])
+        for COMPONENT in self.COMPONENTS:
+            COMPONENT.readFromBus(self.clock)
+        for COMPONENT in self.COMPONENTS:
+            COMPONENT.writeToBus()
+            
+        if self.clock:
+            # high state
+            for COMPONENT in self.COMPONENTS:
+                COMPONENT.read().print(16)
+            self.HALT = True
+        
     def start(self):
         while True:
-            self.clock = False
-            self.lowLevel()
-            self.clock = True
-            self.risingEdge()
-            self.clock = True
-            self.highLevel()
-            self.clock = False
-            self.fallingEdge()
+            if self.HALT:
+                break
+            self.update()
+            self.clock = not self.clock
             time.sleep(1/self.clockRate)
             
 computer = Computer(10)
