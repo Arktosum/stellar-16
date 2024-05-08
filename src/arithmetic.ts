c41849a2393beef73e6ec7c39d8b24e925471f32
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-05-08 15:55:25 +0530

    feature : seven segment display

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -1,5 +1,4 @@
-import { AND, OR, XOR } from "./gates";
-
+import { AND, AND3, NAND, NOT, OR, XOR } from "./gates";
 
 function HALF_ADDER(A : boolean, B: boolean) : [boolean,boolean]{
     const SUM = XOR(A,B);
@@ -29,3 +28,44 @@ export function nADDER(A : boolean[], B : boolean[], C : boolean) : [boolean[],b
     SUMS = SUMS.reverse()
     return [SUMS,CARRY];
 }
+
+function SOP(products : boolean[]) : boolean{
+    let ans = false;
+    for(let product of products){
+        ans = OR(ans,product);
+    }
+    return ans;
+}
+
+    // WORK IN PROGRESS
+export function HEX_DISPLAY(booleanArray : boolean[]) : boolean[]{
+    if(booleanArray.length != 4){
+        throw Error("Invalid input size in hex display!");
+    }
+
+    let [A,B,C,D] = booleanArray
+    let [NA,NB,NC,ND] = [NOT(A),NOT(B),NOT(C),NOT(D)]
+    let products_a = [AND(NB,ND),AND(NA,C),AND(B,C),AND(A,ND),AND3(NA,B,D),AND3(A,NB,NC)]
+    let products_b = [AND(NA,NB),AND(NB,ND),AND3(NA,NC,ND),AND3(NA,C,D),AND3(A,NC,D)]
+    let products_c = [AND(NA,NC),AND(NA,D),AND(NC,D),AND(NA,B),AND(A,NB)]
+    let products_d = [AND(A,NC),AND3(NA,NB,ND),AND3(NB,C,D),AND3(B,NC,D),AND3(B,C,ND)]
+    let products_e = [AND(NB,ND),AND(C,ND),AND(A,C),AND(A,B)]
+    let products_f = [AND(NC,ND),AND(B,ND),AND(A,NB),AND(A,C),AND3(NA,B,NC)]
+    let products_g = [AND(NB,C),AND(C,ND),AND(A,NB),AND(A,D),AND3(NA,B,NC)]
+
+    const a  = SOP(products_a);
+    const b  = SOP(products_b);
+    const c  = SOP(products_c);
+    const d  = SOP(products_d);
+    const e  = SOP(products_e);
+    const f  = SOP(products_f);
+    const g  = SOP(products_g);
+
+    // a , b, c, d, e , f, g
+    return [a,b,c,d,e,f,g];    
+}
+
+
+
+
+// 0011111011110111
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,45 @@
-import { nADDER } from "./arithmetic";
+import { HEX_DISPLAY, nADDER } from "./arithmetic";
 import { Clock } from "./clock";
 import { Counter } from "./memory";
 import { Numeric } from "./Numeric";
 
+class HexDisplay{
+    element: HTMLDivElement;
+    constructor(){
+        this.element = document.createElement("div");
+        document.body.appendChild(this.element);
+        this.displayNumber(Numeric.getBinaryArray("0000"));
+    }
+    displayNumber(booleanArray : boolean[]){
+        let display = HEX_DISPLAY(booleanArray);
+        let [A,B,C,D,E,F,G] = display.map((item)=>{
+            return item ? "yellow" : '#3a3a3a'
+        }) 
+        this.element.innerHTML = `
+        <div style="background-color:black; width : 100px; height:150px; padding : 5px; display:flex; flex-direction:column; align-items:center">
+            <div id="a" style="background-color:${A}; width : 60px; height : 20px; border-radius: 10px;"></div>
+            <div style="display:flex; justify-content:space-between; width:100%">
+                <div id="f" style="background-color:${F}; width : 20px; height : 40px; border-radius: 10px;"></div>
+                <div id="b" style="background-color:${B}; width : 20px; height : 40px; border-radius: 10px;"></div>
+            </div>
+            <div id="g" style="background-color:${G}; width : 60px; height : 20px; border-radius: 10px;"></div>
+            <div style="display:flex; justify-content:space-between; width:100%">
+                <div id="e" style="background-color:${E}; width : 20px; height : 40px; border-radius: 10px;"></div>
+                <div id="c" style="background-color:${C}; width : 20px; height : 40px; border-radius: 10px;"></div>
+            </div>
+            <div id="d" style="background-color:${D}; width : 60px; height : 20px; border-radius: 10px;"></div>
+        </div>
+    `
+    }
+}
+
+const hexDisplayA = new HexDisplay();
+const hexDisplayB = new HexDisplay();
+const hexDisplayC = new HexDisplay();
+const hexDisplayD= new HexDisplay();
 
 
-const clock = new Clock(100);
+const clock = new Clock(20);
 
 
 
@@ -14,10 +48,19 @@ clock.startClock(pulse,pulseStart);
 
 const counter = new Counter(16);
 
-
 function pulseStart(){
     const ans = Numeric.fromBinaryArray(counter.output())
-    console.log(ans.binaryString,ans.decimalString);
+
+    let nibbleA = ans.binaryArray.slice(0,4);
+    let nibbleB = ans.binaryArray.slice(4,8);
+    let nibbleC = ans.binaryArray.slice(8,12);
+    let nibbleD = ans.binaryArray.slice(12,16);
+
+    hexDisplayA.displayNumber(nibbleA)
+    hexDisplayB.displayNumber(nibbleB)
+    hexDisplayC.displayNumber(nibbleC)
+    hexDisplayD.displayNumber(nibbleD)
+    // console.log(ans.binaryString,ans.decimalString);
 }
 function pulse(){
     counter.ASYNC(true,clock.clock);
